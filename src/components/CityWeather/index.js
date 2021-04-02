7cd058c3ce03b21d0cd05f7900285fd8937f35a9
Saurabh Shukla <73607833+shukla9045@users.noreply.github.com>
2021-04-02 17:57:16 +0530

    updated

--- src/components/CityWeather/index.js
+++ src/components/CityWeather/index.js
@@ -18,8 +18,8 @@ const cityWheather = ({
           <h1>Name : {name}</h1>
           <h1>Temperature(in K) : {temp}</h1>
           <h1>Description : {description}</h1>
-          <h1>Minimum temp : {temp_min}</h1>
-          <h1>Maximum temp : {temp_max}</h1>
+          <h1>Min temp : {temp_min}</h1>
+          <h1>Max temp : {temp_max}</h1>
           <h1>Humidity : {humidity}</h1>
           <h1>Wind Speed : {windspeed}</h1>
         </>
